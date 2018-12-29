c1467712624a069a625fe408827a75902b413dfb
Harrison <hichyen1207@gmail.com>
2018-12-29 16:12:28 +0800

    add heroku setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,7 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hant'
+# LANGUAGE_CODE = 'zh_Hant'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Taipei'
 
